a56af87415dfc8ba6bcbe33958284355db3fc871
fanes-setiawan <fanessetiawan.1401@gmail.com>
2024-02-26 22:32:58 +0700

    first commit

---
+++ lib/home/view/home_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: const Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
